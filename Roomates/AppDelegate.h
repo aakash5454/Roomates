f6b4f4db07e09ebd66d6c53d1a184702cc3859b4
aakash5454 <aakash.p@coppermobile.com>
2015-08-10 01:05:35 -0500

    Initial Commit

---
+++ Roomates/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Roomates
+//
+//  Created by Sky on 8/10/15.
+//  Copyright (c) 2015 Sky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
